ff2d9b9bb59eb5f97626d6487ab330f1aacb2764
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-20 20:32:45 +0000

    pascals triangle has been configured

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,18 +3,27 @@
 
 
 def pascal_triangle(n):
-    """calculates pascal nd returns a list"""
-    pascal_list = []
-
+    """
+    calculates pascals triangle for the a list
+    """
+    if type(n) is not int:
+        raise TypeError("n must be an interger")
+    triangle = []
     if n <= 0:
-        return pascal_list
-
-    for i in range(n):
-        temp_list= []
-        for j in range(i + 1):
-            if (j == 0 or j == i):
-                temp_list.append(1)
-            else:
-                temp_list.append(pascal_list[i-1][j-1 + pascal_list[i-1][j]])
-        pascal_list.append(temp_list)
-    return pascal_list
+        return triangle
+    previous = [1]
+    for row_index in range(n):
+        rowlist = []
+        if row_index == 0:
+            rowlist = [1]
+        else:
+            for i in range(row_index + 1):
+                if 1 == 0:
+                    rowlist.append(0 + previous[i])
+                elif i == (row_index):
+                    rowlist.append(previous[i - 1] + 0)
+                else:
+                    rowlist.append(previous[i - 1] + previous[1])
+        previous = rowlist
+        triangle.append(rowlist)
+    return triangle
