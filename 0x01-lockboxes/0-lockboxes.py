2786f02c4aa84c096ac69aa3812cb026977083a3
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-25 12:53:18 +0000

    lockboxes complete

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""
-Task 0: Lockboxes
-"""
+""" Module that checks locked boxes """
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened
-    """
-    if boxes is None or len(boxes) is 0:
+    """ Method that determines if all boxes can be opened """
+
+    if boxes == 0:
         return False
 
-    status = ["T"]
-
-    for box in range(1, len(boxes)):
-        status.append("F")
-
-    for box in range(0, len(boxes)):
-        if (status[box] is "T" or box is 0):
-            for key in boxes[box]:
-                if int(key) < len(boxes) and status[key] is "F":
-                    for k in boxes[key]:
-                        if k < len(boxes):
-                            status[k] = "T"
-                if key < len(boxes):
-                    status[key] = "T"
-
-    if "F" in status:
+    if not isinstance(boxes, list):
         return False
-    return True
+
+    if len(boxes) == 0:
+        return False
+
+    check = [0]
+    list_ing = [i for i in range(len(boxes))]
+
+    for in_check in check:
+        for in_boxes in boxes[in_check]:
+            if in_boxes not in check and in_boxes in list_ing:
+                if in_boxes >= len(boxes):
+                    return False
+                check.append(in_boxes)
+
+    if len(check) == len(boxes):
+        return True
+    else:
+        return False
