bee4b7c5872b5713d309b4751ea320ae87fad778
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-27 11:10:28 +0000

    lockboxes complete

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,65 @@
 #!/usr/bin/python3
-"""
-You have and n number in front of you.
-Each box is numbered sequentially
-from 0 to n - 1 and eaxh box may
-Contain keys to other boxes
-"""
-def join(T, R):
-  res =[]
-  for e in R:
-    res += T[e]
-  return res
+def canUnlockAll(boxes):   
+    if not boxes or ( type(boxes) != list):
+        return False
+    keyList=[]
+    hasMoreKeys = False
+    countBoxes = len(boxes)
+    boxesUnlock = setBoxesUnlock(countBoxes)
+    for i in range(countBoxes):
+        if( type(boxes[i]) == list ):
+            if i == 0:
+                keyList.append(0)
+                setKeys(boxes[i], keyList,boxesUnlock)
+            if boxesUnlock[i] == 0 and keyList.count(i) > 0:
+                keyList = unlockBox(keyList,boxesUnlock,boxes)  
+        else:
+            return False
+    if boxesUnlock.count(0) == 0:
+        return True
+    return False
 
-def canUnlockAll(boxes):
-  index = 0
-  total = list(set(boxes[0]) | {0})
-  added = True
-  while added:
-    added = False
-    for j in join(boxes,total[indexe:]):
-      if j not in total:
-        total.append(j)
-        index +=1
-        added= True
-  print(total)
+def unlockBox(keys,boxesUnlock,boxes):
+    keys_copy = []
+    if len(keys) >0:
+        if keys[0] < len(boxesUnlock):
+            boxesUnlock[keys[0]] = 1
+            if not boxes[keys[0]] or ( type(boxes[keys[0]]) == list):
+                setKeys(boxes[keys[0]],keys,boxesUnlock)
+            keys.pop(0)
+            unlockBox(keys,boxesUnlock,boxes)
+    return keys
 
-  return len(total)==len(boxes)
+def setKeys(box, keyList,boxesUnlock):
+    if box:
+        for key in box:
+            if isInteger(key):
+                if key < len(boxesUnlock) and keyList.count(key) == 0 and boxesUnlock[key] == 0:
+                    keyList.append(key) 
+
+def setBoxesUnlock(count):
+    boxes = []
+    for i in range(count):
+        boxes.append(0)
+    return boxes 
+        
+def isInteger(str):
+    is_int = True
+    try:
+        int(str)
+    except ValueError:
+        is_int = False
+
+    return is_int        
+        
+#  X    X    X    X    X
+#[[1], [2], [3], [4], []]
+#  0    1    2    3   4
+
+#     X       X       X          X       X     X      X
+#[[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+#     0       1       2          3       4      5     6
+
+#  X       X       X            X
+#[[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+#   0      1       2       3    4     5       6
