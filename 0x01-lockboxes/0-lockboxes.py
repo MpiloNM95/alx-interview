4f5bd9ff2b9bf90d5a6d914918b3363547456f58
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-27 19:47:29 +0000

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,15 +15,13 @@ def caUnlockAll(boxes):
     - True if all boxes can be opened. False otherwise
     """
     n = len(boxes)
-    unlocked_boxes = set([0]) # Keeps track of boxes that have been unlocked so far
-    keys = set(boxes[0]) # Keys initially available to unlock other boxes
+    unlocked_boxes = set([0])
+    keys = set(boxes[0]) 
 
-    
-    # Keep on trying to unlock more boxes until the boxes are unlocked all of them or until the boxes cant be unlock anymore
     while keys and len(unlocked_boxes) < n:
         key = keys.pop()
-        if key < n and key not in unlocked_boxes: # Check if the key opens a box we haven't unlocked yet 
+        if key < n and key not in unlocked_boxes: 
             unlocked_boxes.add(key)
-            keys.update(boxes[key]) # Add the key in the unlocked box to the collection of available keys
+            keys.update(boxes[key]) 
 
     return len(unlocked_boxes) == n
