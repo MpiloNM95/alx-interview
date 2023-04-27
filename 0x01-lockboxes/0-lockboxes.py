8a3960bc832fd2b87bfb020f1ac9a933589f06c8
MpiloNM95 <mpilonmafu95@gmail.com>
2023-04-27 19:27:57 +0000

    lockboxes complete

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,20 +6,24 @@ Solution for the locboxes problem
 
 def caUnlockAll(boxes):
     """
-    To see if all locked boxes can be opened using 
-    the code below.
-    """
-    if (type(boxes)) is not list:
-        return False
-    elif (len(boxes)) == 0:
-        return False
+    Determine if all boxes can be opened by checking if there is a path from box 0 to all other boxes.
 
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for ids in range(len(boxes)):
-            boxes_checked = k in boxes[ids] and k != ids
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
-    return True
+    Args:
+    _ boxes: A list of lists of integers. Each inner list represents a box and contains the indices of the boxes it can open.
+
+    Returns: 
+    - True if all boxes can be opened. False otherwise
+    """
+    n = len(boxes)
+    unlocked_boxes = set([0]) # Keeps track of boxes that have been unlocked so far
+    keys = set(boxes[0]) # Keys initially available to unlock other boxes
+
+    
+    # Keep on trying to unlock more boxes until the boxes are unlocked all of them or until the boxes cant be unlock anymore
+    while keys and len(unlocked_boxes) < n:
+        key = keys.pop()
+        if key < n and key not in unlocked_boxes: # Check if the key opens a box we haven't unlocked yet 
+            unlocked_boxes.add(key)
+            keys.update(boxes[key]) # Add the key in the unlocked box to the collection of available keys
+
+    return len(unlocked_boxes) == n
