e67eb04c2441823210977c416ff462fc86b68d5f
MpiloNM95 <mpilonmafu95@gmail.com>
2023-05-04 12:06:12 +0000

    minimum operations done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,14 +12,21 @@ def minOperations(n):
         Args: if n is impossible to achieve
         Return: 0
     """
-    if n <= 0:
+    if n <= 1:
         return 0
-    num_h = 1
+
     num_ops = 0
+    num_h = 1
+    clipboard = 0
+
     while num_h < n:
+        if n % num_h == 0:
+            clipboard = num_h
+            num_ops += 1
         num_ops += 1
-        num_h *= 2
-    if num_h == n:
-        return num_ops
-    else:
-        return 0
+        num_h += clipboard
+
+        if num_h == n:
+            break
+
+    return num_ops
