40fd781b278e466c1a69d4c11c7a4d88563936c5
MpiloNM95 <mpilonmafu95@gmail.com>
2023-05-09 13:18:25 +0000

    log parsing done

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+A script that reads stdin by line and computes metrics
+"""
+import sys
+
+
+line_count = 0
+total_size = 0
+status_code_counts = {}
+
+
+def print_statistics():
+    print(f"Total file size: {total_size}")
+    for code in sorted(status_code_counts):
+        if status_code_counts[code] > 0:
+            print(f"{code}: {status_code_counts[code]}")
+    print()
+
+
+def process_line(line):
+    global line_count, total_size, status_code_counts
+    line_count += 1
+    if line_count > 10:
+        print_statistics()
+        line_count = 1
+    parts = line.split()
+    if len(parts) != 10:
+        return
+    try:
+        code = int(parts[8])
+        size = int(parts[9])
+    except ValueError:
+        return
+    total_size += size
+    status_code_counts[code] = status_code_counts.get(code, 0) + 1
+
+try:
+    for line in sys.stdin:
+        process_line(line)
+
+
+except KeyboardInterrupt:
+    pass
+
+
+print_statistics()
+
+if __name__ == '__main__':
+    process_line
--- 0x03-log_parsing/README.md
+++ 0x03-log_parsing/README.md
@@ -1,2 +1,11 @@
 # 0x03. Log Parsing
 ```Algorithm``` | ```Python```
+--- 
+* Allowed editors: vi, vim, emacs
+* All files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+* All files should end with a new line
+* The first line of all files should be exactly #!/usr/bin/python3
+* A README.md file, at the root of the folder of the project, is mandatory
+* Code should use the PEP 8 style (version 1.7.x)
+* All your files must be executable
+* The length of your files will be tested using wc
