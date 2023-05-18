6a7c1824ff87b498818e34eae8e3966b7d899f6e
MpiloNM95 <mpilonmafu95@gmail.com>
2023-05-18 17:34:56 +0000

    fixed?

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,35 +8,24 @@ def validUTF8(data):
     """
     Return: True if data is a valid UTF-8 encoding, else return False
     """
-    # Number of bytes in the current character
     num_bytes = 0
-
     for num in data:
-        # Check if the current number is a valid leading byte
         if num_bytes == 0:
-            # Count the number of leading ones (MSB)
             mask = 1 << 7
             while mask & num:
                 num_bytes += 1
                 mask >>= 1
-
-            # If no leading ones, it's a single-byte character
             if num_bytes == 0:
                 continue
-
-            # Invalid leading byte
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # Check if the current number is a valid continuation byte
             if not (num >> 6) == 0b10:
                 return False
-
-        # Decrease the remaining bytes to process for the current character
         num_bytes -= 1
-
-    # All bytes were validated
     return num_bytes == 0
+
+if __name__ == '__main__':
 
 data = [65]
 print(validUTF8(data))
